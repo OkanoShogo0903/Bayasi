f01929c3c4dcd97c326b76cc1e5d981a6129ac7f
okano_shogo <b1546842@planet.kanazawa-it.ac.jp>
2018-02-04 13:52:28 +0900

    edit README for markdown

--- siege.py
+++ siege.py
@@ -1,14 +1,18 @@
 import requests
 import json
+# TODO : 
+#   json file ,save and load
+#   print score diff 
 def requestPlayer(username, platform):
     r = requests.get('https://api.r6stats.com/api/v1/players/{}?platform={}'.format(username, platform))
-    json = r.json()
+    dict_ = r.json()
 
     if r.status_code == 404:
         return None
     else:
-        return json
+        return dict_
 
 if __name__ == '__main__':
     data = requestPlayer("DarkKnight_haku","uplay")
-    print(data)
+    print('json : {}'.format(type(data)))
+#    print(data.)
