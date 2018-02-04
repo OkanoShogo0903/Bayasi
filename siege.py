ef2074d116421b9cf2e886a758bc35aef4eabf7c
okano_shogo <b1546842@planet.kanazawa-it.ac.jp>
2018-02-04 14:38:25 +0900

    差分を画面出力するようにした、要テスト

--- siege.py
+++ siege.py
@@ -1,8 +1,15 @@
 import requests
 import json
+import time
+import threading
 # TODO : 
 #   json file ,save and load
 #   print score diff 
+# 機能 :
+#   今日のKDと勝ち負けの数、ヘッドショット率、プレイ時間を表示
+username = "syababa"
+platform = "uplay"
+
 def requestPlayer(username, platform):
     r = requests.get('https://api.r6stats.com/api/v1/players/{}?platform={}'.format(username, platform))
     dict_ = r.json()
@@ -12,7 +19,34 @@ def requestPlayer(username, platform):
     else:
         return dict_
 
+def Thread():
+    now = datetime.now()
+    global username,platform
+    global before
+    # 記録
+    if now.hour == 12 and now.minute == 0:
+        data = requestPlayer(username,platform)
+        before = data
+
+    # 差分の取得と表示
+    if(now.hour == 1 and now.minute == 0):
+        data = requestPlayer(username,platform)
+        player = data['player']
+        d_win   = player['stats']['ranked']['wins'] - before['stats']['ranked']['wins']
+        d_loss  = player['stats']['ranked']['losses'] - before['stats']['ranked']['losses']
+        d_kill  = player['stats']['ranked']['kills'] - before['stats']['ranked']['kills']
+        d_death = player['stats']['ranked']['deaths'] - before['stats']['ranked']['deaths']
+        d_head  = player['stats']['overrall']['headshots'] - before['stats']['overrall']['headshots']
+        d_time  = player['stats']['ranked']['playtime'] - before['stats']['ranked']['playtime']
+        
+        print('勝敗レート : {}'.format(d_win/d_loss))
+        print('KD : {}'.format(d_kill/d_death))
+        print('headshot : {}\%'.format( (headshots/d_kill)*100 ))
+        print('プレイ時間 : {} (minute)'.format(d_time))
+
+    t=threading.Timer(60,Thread)
+    t.start()
+
 if __name__ == '__main__':
-    data = requestPlayer("DarkKnight_haku","uplay")
-    print('json : {}'.format(type(data)))
-#    print(data.)
+#    data = requestPlayer("DarkKnight_haku","uplay")
+    Thread()
