60b22ea5a7950cf6b742a5a852253bc5eff32f22
okano_shogo <b1546842@planet.kanazawa-it.ac.jp>
2018-02-05 12:51:00 +0900

    シージの情報をツイートするようにした。python siege.py で起動

--- bayasi.py
+++ bayasi.py
@@ -6,7 +6,6 @@ sys.path.append("C:\\Users\\okano\\Anaconda3\\Lib\\site-packages")
 from datetime import datetime
 import locale
 import wave
-import pyaudio
 import time
 import threading
 import random
@@ -27,41 +26,39 @@ text = ["しーじ",
 #"公式URL"
 ]
 
-def twitter_func():
-    CK = 'Ie5H016sEX1S7rPa2H1xDzhjP'                             # Consumer Key
-    CS = 'pcWF2njdauUm2t3g5Pq4l2XxaPZOB1DRU6CVSsUlj64psVqVRJ'         # Consumer Secret
-    AT = '908126063284199425-ObKYdsMWZVL0dTNlSVr5f6qm5ZonQcf' # Access Token
-    AS = 'v3UV7IwKbGUJ0Qdg1HTtO8YZOjaLwV11AsaFbigmpeqkn'         # Accesss Token Secert
+CK = 'Ie5H016sEX1S7rPa2H1xDzhjP'                             # Consumer Key
+CS = 'pcWF2njdauUm2t3g5Pq4l2XxaPZOB1DRU6CVSsUlj64psVqVRJ'         # Consumer Secret
+AT = '908126063284199425-ObKYdsMWZVL0dTNlSVr5f6qm5ZonQcf' # Access Token
+AS = 'v3UV7IwKbGUJ0Qdg1HTtO8YZOjaLwV11AsaFbigmpeqkn'         # Accesss Token Secert
 
-    # ツイート投稿用のURL
-    url = "https://api.twitter.com/1.1/statuses/update.json"
+# OAuth認証で POST method で投稿
+twitter = OAuth1Session(CK, CS, AT, AS)
 
-    # ツイート本文
-    rand_int = random.randint(0,len(text)-1)
-    hoge = text[rand_int]
-    print(hoge)
+# ツイート投稿用のURL
+url = "https://api.twitter.com/1.1/statuses/update.json"
+
+def tweet(hoge):
+    global url
+    global twitter
     params = {"status": hoge}
-#    params = {"status": "Hello, World!"}
-
-    # OAuth認証で POST method で投稿
-    twitter = OAuth1Session(CK, CS, AT, AS)
     req = twitter.post(url, params = params)
 
     # レスポンスを確認
     if req.status_code == 200:
-        print (text[rand_int])
+        print ('OK: {0}'.format(hoge))
     else:
         print ("Error: %d" % req.status_code)
 
 def bot_timer():
     b = datetime.now()
-    if(b.minute%10 == 0): # x分ごとの時報
-        twitter_func()
+#    if(b.minute%10 == 0): # x分ごとの時報
+    if(b.minute == 0): # 一時間ごとの時報
+        rand_int = random.randint(0,len(text)-1)
+        tweet(text[rand_int])
     # 呼び出された後の処理
     # 指定秒に呼び出されるようにスレッドをセットする
     t=threading.Timer(60,bot_timer)
     t.start()
 
-# main function
-t=threading.Timer(1,bot_timer)
-t.start()
+if __name__ == '__main__':
+    bot_timer()
--- siege.py
+++ siege.py
@@ -3,6 +3,7 @@ import json
 import time
 import threading
 from datetime import datetime
+import bayasi
 # TODO : 
 #   json file ,save and load
 #   print score diff 
@@ -20,34 +21,61 @@ def requestPlayer(username, platform):
     else:
         return dict_
 
+def Siege():
+    data = requestPlayer(username,platform)
+    player = data['player']
+    d_win   = player['stats']['ranked']['wins'] - before['stats']['ranked']['wins']
+    d_loss  = player['stats']['ranked']['losses'] - before['stats']['ranked']['losses']
+    d_kill  = player['stats']['ranked']['kills'] - before['stats']['ranked']['kills']
+    d_death = player['stats']['ranked']['deaths'] - before['stats']['ranked']['deaths']
+    d_head  = player['stats']['overall']['headshots'] - before['stats']['overall']['headshots']
+    d_time  = player['stats']['ranked']['playtime'] - before['stats']['ranked']['playtime']
+ 
+    if d_time == 0:
+        print("did not play game")
+        return
+    else:
+        text = ""
+
+        print('WIN : {0} ,LOSS: {1}'.format(d_win,d_loss))
+        try:
+            print('KD : {}'.format(d_kill/d_death))
+        except:
+            pass
+        try:
+            print('headshot : {}\%'.format( (headshots/d_kill)*100 ))
+        except:
+            pass
+        print('プレイ時間 : {} (minute)'.format(d_time))
+        
+        text += "WIN : " + str(d_win) + " , LOSS : " + str(d_loss) + "\n"
+        try:
+            text += "KD : " + str(d_kill/d_death) + "\n"
+        except:
+            pass
+        try:
+            text += "HeadShot : " + str( (headshots/d_kill)*100 ) + "\n"
+        except:
+            pass
+        text += "PlayTime : " + str(d_time) + "\n"
+
+        bayasi.tweet(text)
+
 def Thread():
     now = datetime.now()
     global username,platform
     global before
     # 記録
-#    if now.hour == 12 and now.minute == 0:
-    if now.minute == 0:
+    if now.hour == 12 and now.minute == 0:
         data = requestPlayer(username,platform)
         before = data
 
     # 差分の取得と表示
-#    if(now.hour == 1 and now.minute == 0):
-    if(now.minute == 30):
-        data = requestPlayer(username,platform)
-        player = data['player']
-        d_win   = player['stats']['ranked']['wins'] - before['stats']['ranked']['wins']
-        d_loss  = player['stats']['ranked']['losses'] - before['stats']['ranked']['losses']
-        d_kill  = player['stats']['ranked']['kills'] - before['stats']['ranked']['kills']
-        d_death = player['stats']['ranked']['deaths'] - before['stats']['ranked']['deaths']
-        d_head  = player['stats']['overrall']['headshots'] - before['stats']['overrall']['headshots']
-        d_time  = player['stats']['ranked']['playtime'] - before['stats']['ranked']['playtime']
-        
-        print('勝敗レート : {}'.format(d_win/d_loss))
-        print('KD : {}'.format(d_kill/d_death))
-        print('headshot : {}\%'.format( (headshots/d_kill)*100 ))
-        print('プレイ時間 : {} (minute)'.format(d_time))
+    if(now.hour == 1 and now.minute == 0):
+#    if(now.minute == 30):
+        Siege()
 
-    print("threa active minute : {}".format(now.minute))
+#    print("Siege Thread Active ... minute : {}".format(now.minute))
     t=threading.Timer(60,Thread)
     t.start()
 
@@ -56,3 +84,4 @@ if __name__ == '__main__':
     before = d['player']
 #    data = requestPlayer("DarkKnight_haku","uplay")
     Thread()
+    bayasi.bot_timer()
